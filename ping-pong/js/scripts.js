ccfbf6ad08a26d0ac4360ef8474b6a445d087a08
Patrick Lipscomb <patrick@>
2016-06-10 11:41:05 -0700

    add html features and basic js to create a game.

--- ping-pong/js/scripts.js
+++ ping-pong/js/scripts.js
@@ -3,21 +3,48 @@ $(document).ready(function() {
     event.preventDefault();
     list = [];
     $('li.temp').remove();
-    userInput = $('input#number').val();
+    var userInput = $('input#number').val();
     pingpong(userInput);
   });
 });
 
+$(document).ready(function() {
+  $('form#randInt').submit(function(event) {
+    event.preventDefault();
+    list = [];
+    $('li.temp').remove();
+    var randomNumber = Math.floor(500 * Math.random());
+    $('#randomn').text(randomNumber);
+  });
+});
+
+$(document).ready(function() {
+  $('form#numberGuesser').submit(function(event) {
+    event.preventDefault();
+    list = [];
+    $('li.temp').remove();
+    var userInput1 = $('input#guess1').val();
+    var userInput2 = $('input#guess2').val();
+    var userInput3 = $('input#guess3').val();
+  });
+});
+
 var list = [];
+var pingCounter = 0
+var pongCounter = 0
+var pingpongCounter = 0
 var pingpong = function(input) {
   var number = parseInt(input);
   for (var i = 1; i <= number; i++) {
     if ( i % 15 === 0 ) {
-      list.push('pingpong');
+      list.push('ping-pong');
+      pingpongCounter += 1
     } else if ( i % 5 === 0 ) {
       list.push('pong');
+      pongCounter += 1
     } else if ( i % 3 === 0) {
       list.push('ping');
+      pingCounter += 1
     } else {
       list.push(i);
     }
