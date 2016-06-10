7a0a8205e819cd4fe7634fe412a8b6b4a70c4677
Patrick Lipscomb <patrick@>
2016-06-10 15:30:16 -0700

    finish

--- ping-pong/js/scripts.js
+++ ping-pong/js/scripts.js
@@ -12,53 +12,30 @@ $(document).ready(function() {
     pingpong(userInput);
     for (var i = 0; i < list.length; i += 1) {
       if ( i < 40 ) {
-        list.forEach(function(item) {
-          $('ul#result1').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result1').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 80 ) {
-        list.forEach(function(item) {
-          $('ul#result2').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result2').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 120 ) {
-        list.forEach(function(item) {
-          $('ul#result3').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result3').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 160 ) {
-        list.forEach(function(item) {
-          $('ul#result4').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result4').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 200 ) {
-        list.forEach(function(item) {
-          $('ul#result5').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result5').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 240 ) {
-        list.forEach(function(item) {
-          $('ul#result6').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result6').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 280 ) {
-        list.forEach(function(item) {
-          $('ul#result7').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result7').append('<li class="temp">' + list[i] + '</li>');
+          $('#hidden').show();
       } else if ( i < 320 ) {
-        list.forEach(function(item) {
-          $('ul#result8').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result8').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 360 ) {
-        list.forEach(function(item) {
-          $('ul#result9').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result9').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 400 ) {
-        list.forEach(function(item) {
-          $('ul#result10').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result10').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 440 ) {
-        list.forEach(function(item) {
-          $('ul#result11').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result11').append('<li class="temp">' + list[i] + '</li>');
       } else if ( i < 480 ) {
-        list.forEach(function(item) {
-          $('ul#result12').append('<li class="temp">' + item[i] + '</li>')
-        });
+          $('ul#result12').append('<li class="temp">' + list[i] + '</li>');
       }
     }
   });
@@ -71,7 +48,6 @@ $(document).ready(function() {
     pingCounter = 0;
     pongCounter = 0;
     pingpongCounter = 0;
-    $('li.temp').remove();
     var randomNumber = Math.floor(500 * Math.random());
     $('#randomn').text(randomNumber);
     pingpong(randomNumber);
@@ -79,7 +55,6 @@ $(document).ready(function() {
   $('form#numberGuesser').submit(function(event) {
     event.preventDefault();
     list = [];
-    $('li.temp').remove();
     var userInput1 = parseInt($('input#guess1').val());
     var userInput2 = parseInt($('input#guess2').val());
     var userInput3 = parseInt($('input#guess3').val());
@@ -98,23 +73,6 @@ $(document).ready(function() {
     $('li#pingpong').text("There are " + pingpongCounter + " pingpongs.");
   })
 });
-
-// $(document).ready(function() {
-//   $('form#numberGuesser').submit(function(event) {
-//     event.preventDefault();
-//     list = [];
-//     $('li.temp').remove();
-//     var userInput1 = $('input#guess1').val();
-//     var userInput2 = $('input#guess2').val();
-//     var userInput3 = $('input#guess3').val();
-//     debugger;
-//     if ( userInput1 === pingCounter && userInput2 === pongCounter && userInput3 === pingpongCounter ) {
-//       $('h2#guesser').text("Wow, great guess! You nailed it.")
-//     } else {
-//       $('h2#guesser').text("Missed it, better luck next time brotato.")
-//     }
-//   });
-// });
 
 var pingpong = function(input) {
   pingCounter = 0;
